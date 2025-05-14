b3711c8426b05827b755faafe0ffee562aa43b3d
ilmi's <112762901+zulilmiihsn@users.noreply.github.com>
2025-05-15 00:05:53 +0800

    Fix font tidak ditemukan pada Typhography.kt

--- app/src/main/java/com/example/omsetku/ui/components/Typhography.kt
+++ app/src/main/java/com/example/omsetku/ui/components/Typhography.kt
@@ -7,7 +7,5 @@ import com.example.omsetku.R
 
 val Poppins = FontFamily(
     Font(R.font.poppins, FontWeight.Normal),
-    Font(R.font.poppins_medium, FontWeight.Medium),
-    Font(R.font.poppins_semibold, FontWeight.SemiBold),
     Font(R.font.poppins_bold, FontWeight.Bold)
 )
