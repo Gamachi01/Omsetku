9a4fc50905fc62c1562c4521b5d7b5ebefdfade0
ilmi's <112762901+zulilmiihsn@users.noreply.github.com>
2025-05-26 18:31:43 +0800

    Fix JVM 21 error dan update gradle

--- app/src/main/java/com/example/omsetku/ui/screen/ReportScreen.kt
+++ app/src/main/java/com/example/omsetku/ui/screen/ReportScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
