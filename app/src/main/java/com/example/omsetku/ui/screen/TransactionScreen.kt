9ec38806bb48b94c5fcea48bb259d1f9aa1833df
ilmi's <112762901+zulilmiihsn@users.noreply.github.com>
2025-06-09 20:43:04 +0800

    fix: perbaikan import FirestoreRepository di TransactionScreen

--- app/src/main/java/com/example/omsetku/ui/screen/TransactionScreen.kt
+++ app/src/main/java/com/example/omsetku/ui/screen/TransactionScreen.kt
@@ -35,6 +35,8 @@ import com.example.omsetku.viewmodels.TransactionViewModel
 import com.example.omsetku.ui.components.FormField
 import com.example.omsetku.ui.components.StandardTextField
 import com.example.omsetku.ui.components.MultilineTextField
+import com.example.omsetku.viewmodels.TransactionViewModelFactory
+import com.example.omsetku.firebase.FirestoreRepository
 
 enum class TransactionType {
     INCOME, EXPENSE
@@ -44,7 +46,7 @@ enum class TransactionType {
 @Composable
 fun TransactionScreen(
     navController: NavController,
-    transactionViewModel: TransactionViewModel = viewModel()
+    transactionViewModel: TransactionViewModel = viewModel(factory = TransactionViewModelFactory(FirestoreRepository()))
 ) {
     var selectedItem by remember { mutableStateOf("Transaction") }
     var selectedType by remember { mutableStateOf(TransactionType.INCOME) }
